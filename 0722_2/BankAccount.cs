1b1d5cbc2988b695a3c116d090b74f1e134bcccf
ethanyun022 <ethanyun022@gmail.com>
2025-07-22 11:49:14 +0900

    기능: C# 프로퍼티 학습
    - 프로퍼티 정의 및 사용
    - get/set 접근자
    - 자동 구현 프로퍼티

---
+++ 0722_2/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0722_2
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private decimal balance;
+        private DateTime createDate;
+
+        public BankAccount(string accountNumber, decimal balance, DateTime createDate)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+            this.createDate = createDate;
+        }
+
+        // 읽기 전용 프로퍼티(get만 있음)
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+        public DateTime CreateDate
+        {
+            get { return  createDate; }
+        }
+
+        private string password;
+        // 쓰기 전용 프로퍼티(set만 있음) - 거의 사용 하지 않습니다.
+        public string Password
+        {
+            set { password = value; }
+        }
+
+        public decimal Balance
+        { 
+            get { return balance; }
+            private set { balance = value; } // private set: 클래스 내부에서만 설정 가능
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount; // private set 사용
+                Console.WriteLine($"{amount}원 입금 완료");
+            }
+        }
+
+    }
+}
---
+++ 0722_2/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _0722_2
+{
+    // 프로퍼티(Property)
+    // 프로퍼티는 필드에 대한 접근을 제어하는 특별한 메서드
+    // 외부에서는 필드처럼 보이지만, 내부에서는 메서드로 동작합니다.
+
+    // 왜 사용하는가?
+    // 데이터 보호 : 잘못된 값이 들어오는것을 방지
+    // 유효성 검사 : 값을 설정할때 조건 검사
+    // 계산된 값 : 다른 필드들을 기반으로 값을 계산
+    // 캡슐화 : 내부 구현을 숨기면서 안전한 접근 제공
+
+    public class Person {
+        private string name;
+        private int age;
+
+        // Name 프로퍼티
+        public string Name { 
+            // 값을 읽을 때 호출
+            get 
+            { 
+                return name; 
+            }  
+            // 값을 설정할 때 호출
+            set 
+            {
+                name = value; 
+            } 
+        }
+        
+        // Age 프로퍼티
+        public int Age { 
+        
+            get 
+            {
+                return age; 
+            }
+            set 
+            {
+                if (value > 0 && value < 120)
+                {
+                    age = value;
+                } else
+                {
+                    Console.WriteLine($"나이는 1 ~ 120 사이의 값이어야 합니다.");
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "김철수";
+            person.Age = 30;
+
+            Console.WriteLine($"이름: {person.Name}");
+            Console.WriteLine($"나이: {person.Age}");
+
+            person.Age = -10;
+        }
+    }
+}
